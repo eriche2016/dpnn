2c0f4e563b1a5ccd39544cdad4cefea182e12000
eriche2016 <1154646635@qq.com>
2016-09-26 21:36:32 +0800

    Update FireModule.lua

--- FireModule.lua
+++ FireModule.lua
@@ -7,22 +7,30 @@ local FireModule, Parent = torch.class('nn.FireModule', 'nn.Decorator')
 
 function FireModule:__init(nInputPlane, s1x1, e1x1, e3x3, activation)
    self.nInputPlane = nInputPlane
-   self.s1x1 = s1x1
-   self.e1x1 = e1x1
-   self.e3x3 = e3x3
+   -- squeeze module
+   self.s1x1 = s1x1 -- the number of convolutional filters(all will be 1x1) in sequeeze module
+   -- expand module 
+   self.e1x1 = e1x1 -- the number of 1x1 filters in expand module
+   self.e3x3 = e3x3 -- the number of 3x3 filters in expand module
    self.activation = activation or 'ReLU'
 
-   if self.s1x1 > (self.e1x1 + self.e3x3) then
+   if self.s1x1 > (self.e1x1 + self.e3x3) then -- to limit #input channels to 3x3 filters
       print('Warning: <FireModule> s1x1 is recommended to be smaller'..
             ' then e1x1+e3x3')
    end
    
    self.module = nn.Sequential()
+  -- squeeze module 
+  -- nInputPlane, s1x1 output channel, 1 x 1 filters
    self.squeeze = nn.SpatialConvolution(nInputPlane, s1x1, 1, 1)
-   self.expand = nn.Concat(2)
-   self.expand:add(nn.SpatialConvolution(s1x1, e1x1, 1, 1))
+   -- expand module
+   self.expand = nn.Concat(2) -- concatenate along second dimension 
+   -- output: bz x e1x1 x H x W
+   self.expand:add(nn.SpatialConvolution(s1x1, e1x1, 1, 1)) 
+   -- output: bz x e3x3 x H x W
    self.expand:add(nn.SpatialConvolution(s1x1, e3x3, 3, 3, 1, 1, 1, 1))
-
+    -- output of self.expand: bz x (e1x1+e3x3) x H x W  
+  
    -- Fire Module
    self.module:add(self.squeeze)
    self.module:add(nn[self.activation]())
