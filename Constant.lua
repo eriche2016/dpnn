273c28cf05a7febfc6d2721cd2e0a220b1639092
eriche2016 <1154646635@qq.com>
2016-09-26 16:35:31 +0800

    Update Constant.lua

--- Constant.lua
+++ Constant.lua
@@ -8,29 +8,30 @@
 ------------------------------------------------------------------------
 local Constant, parent = torch.class("nn.Constant", "nn.Module")
 
+-- value: torch.Tensor 
 function Constant:__init(value, nInputDim)
    self.value = value
    if torch.type(self.value) == 'number' then
       self.value = torch.Tensor{self.value}
    end
    assert(torch.isTensor(self.value), "Expecting number or tensor at arg 1")
-   self.nInputDim = nInputDim
+   self.nInputDim = nInputDim -- set self.nInputDim to nInputDim to be nInputDim
    parent.__init(self)
 end
 
 function Constant:updateOutput(input)
-   if self.nInputDim and input:dim() > self.nInputDim then
-      local vsize = self.value:size():totable()
-      self.output:resize(input:size(1), table.unpack(vsize))
-      local value = self.value:view(1, table.unpack(vsize))
-      self.output:copy(value:expand(self.output:size())) 
-   else
-      self.output:resize(self.value:size()):copy(self.value)
+   if self.nInputDim and input:dim() > self.nInputDim then -- input has batch dim 
+      local vsize = self.value:size():totable() 
+      self.output:resize(input:size(1), table.unpack(vsize))  -- self.output: bz x table.unpack(vsize)
+      local value = self.value:view(1, table.unpack(vsize))  
+      self.output:copy(value:expand(self.output:size()))  -- expand value to be: bz x table.unpack(vsize) 
+   else -- input doesnot have  batch dim 
+      self.output:resize(self.value:size()):copy(self.value)  -- just output self.value
    end
    return self.output
 end
 
 function Constant:updateGradInput(input, gradOutput)
-   self.gradInput:resizeAs(input):zero()
+   self.gradInput:resizeAs(input):zero()  -- no matter what the input, the output will be a const, so here self.gradInput:zero()
    return self.gradInput
 end
